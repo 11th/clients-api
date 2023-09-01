8fe32a02f617a35087ae293c4ca0e23f1586d01c
11th <11th@bk.ru>
2023-09-01 15:18:36 +0300

    initial commit

---
+++ src/test/java/com/github/th/clientsapi/ClientsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.th.clientsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
